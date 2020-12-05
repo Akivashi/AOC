ad71310901f4edc5b5816e11cd981f709b81d38c
Rene Aparicio <rene@infi.nl>
2020-12-05 15:17:44 +0100

    day5 directly to binary

--- AOC2020/days/day5/Day5Puzzle1.py
+++ AOC2020/days/day5/Day5Puzzle1.py
@@ -1,5 +1,4 @@
 import argparse
-import re
 
 class Day5Puzzle1(object):
   def __init__(self):
@@ -7,25 +6,19 @@ class Day5Puzzle1(object):
     self.puzzle = 1
 
   def solution(self, inputfile):
-    rowrule = re.compile("([FB]{7})([LR]{3})$")
     current_max_seat_id = 0
     with open(inputfile,"r") as file:
       lines = file.read().splitlines()
     for line in lines:
-      seat_info = rowrule.search(line)
-      row_binary = transform_row_to_binary(seat_info.group(1))
-      column_binary = transform_column_to_binary(seat_info.group(2))
-      current_max_seat_id = max(current_max_seat_id, get_seat_id(row_binary, column_binary))
+      current_max_seat_id = max(
+        current_max_seat_id,
+        int(line
+          .replace('F', '0')
+          .replace('B', '1')
+          .replace('L', '0')
+          .replace('R', '1')
+        , 2))
     return current_max_seat_id
-
-def transform_row_to_binary(row_input):
-  return row_input.replace('F', '0').replace('B', '1')
-
-def transform_column_to_binary(column_input):
-  return column_input.replace('L', '0').replace('R', '1')
-
-def get_seat_id(row_binary, column_binary):
-  return int(row_binary, 2) * 8 + int(column_binary, 2)
 
 def main():
   ap = argparse.ArgumentParser()
--- AOC2020/days/day5/Day5Puzzle2.py
+++ AOC2020/days/day5/Day5Puzzle2.py
@@ -1,5 +1,4 @@
 import argparse
-import re
 
 class Day5Puzzle2(object):
   def __init__(self):
@@ -7,15 +6,17 @@ class Day5Puzzle2(object):
     self.puzzle = 2
 
   def solution(self, inputfile):
-    rowrule = re.compile("([FB]{7})([LR]{3})$")
     existing_seat_ids = []
     with open(inputfile,"r") as file:
       lines = file.read().splitlines()
     for line in lines:
-      seat_info = rowrule.search(line)
-      row_binary = transform_row_to_binary(seat_info.group(1))
-      column_binary = transform_column_to_binary(seat_info.group(2))
-      existing_seat_ids.append(get_seat_id(row_binary, column_binary))
+      existing_seat_ids.append(
+        int(line
+              .replace('F', '0')
+              .replace('B', '1')
+              .replace('L', '0')
+              .replace('R', '1')
+            , 2))
     
     existing_seat_ids.sort()
 
@@ -24,15 +25,6 @@ class Day5Puzzle2(object):
       if(seat_id != existing_seat_ids[0] and (seat_id-1) != prev_seat_id):
         return seat_id-1
       prev_seat_id = seat_id
-
-def transform_row_to_binary(row_input):
-  return row_input.replace('F', '0').replace('B', '1')
-
-def transform_column_to_binary(column_input):
-  return column_input.replace('L', '0').replace('R', '1')
-
-def get_seat_id(row_binary, column_binary):
-  return int(row_binary, 2) * 8 + int(column_binary, 2)
 
 def main():
   ap = argparse.ArgumentParser()
